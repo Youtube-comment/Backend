328c5d6cf268561d2727ba8ec78e8de55a579ec2
Jinwoo Kim <zaqaz113@naver.com>
2023-05-15 21:05:32 +0900

    feat:comment insert

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('get-channel-id/', views.get_channel_id),
     path('get-youtube-list/', views.get_youtube_list),
     path('get-comment-list/', views.get_comment_list),
+    path('post-comment-insert/', views.post_comment_insert),
 ]
