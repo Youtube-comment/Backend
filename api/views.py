bbd3bbcb3d6b5314a8d6a1ba6f64c69a9d377aaf
Jinwoo Kim <zaqaz113@naver.com>
2023-05-23 13:36:51 +0900

    [hotfix] 들여쓰기 Error

--- api/views.py
+++ api/views.py
@@ -20,12 +20,11 @@ from django.http import JsonResponse
 def get_channel_id(request):
     token = request
     response = requests.get('https://www.googleapis.com/youtube/v3/channels', params={
-          'access_token': token,
-          'part': 'snippet',
-          'key': 'AIzaSyAVU2_JX41C1c4k3i9V2N5yDEf2_cldpLw',
-          'mine': True,
-          
-     })
+        'access_token': token,
+        'part': 'snippet',
+        'key': 'AIzaSyAVU2_JX41C1c4k3i9V2N5yDEf2_cldpLw',
+        'mine': True,
+    })
     
     channel_id = response.json()
     return channel_id.get('items')[0]['id']
@@ -34,11 +33,11 @@ def get_channel_id(request):
 def get_channel_sb(request):
     token = request
     response = requests.get('https://www.googleapis.com/youtube/v3/channels', params={
-          'access_token': token,
-          'part': 'snippet',
-          'key': 'AIzaSyAVU2_JX41C1c4k3i9V2N5yDEf2_cldpLw',
-          'mine': True,
-     })
+        'access_token': token,
+        'part': 'snippet',
+        'key': 'AIzaSyAVU2_JX41C1c4k3i9V2N5yDEf2_cldpLw',
+        'mine': True,
+    })
     
     subscriber = response.json()
     return JsonResponse({'data' : subscriber})
